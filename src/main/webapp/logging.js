4d25fef92288a9313826c6bcdcc1dce37649d05b
Kara Alexandra <ardnaxelarak@gmail.com>
2019-03-21 21:35:39 -0700

    Replace tabs with spaces in logging.js

--- src/main/webapp/logging.js
+++ src/main/webapp/logging.js
@@ -1,41 +1,41 @@
 function createXHR()
 {
-	var request = false;
-	try
-	{
-		request = new ActiveXObject('Msxml2.XMLHTTP');
-	}
-	catch (err2)
-	{
-		try
-		{
-			request = new ActiveXObject('Microsoft.XMLHTTP');
-		}
-		catch (err3)
-		{
-			try
-			{
-				request = new XMLHttpRequest();
-			}
-			catch (err1)
-			{
-				request = false;
-			}
-		}
-	}
-	return request;
+  var request = false;
+  try
+  {
+    request = new ActiveXObject('Msxml2.XMLHTTP');
+  }
+  catch (err2)
+  {
+    try
+    {
+      request = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    catch (err3)
+    {
+      try
+      {
+        request = new XMLHttpRequest();
+      }
+      catch (err1)
+      {
+        request = false;
+      }
+    }
+  }
+  return request;
 }
 
 function writelog(filename, user)
 {
-	var args = Array.prototype.slice.call(arguments, 2);
-	var xhr=createXHR();
-	var parameters="file=" + filename + "&name=" + user.trim()
-	for (var i = 0; i < args.length; i++)
-		parameters += "&a_" + i + "=" + args[i];
-	xhr.open("POST", "logger.php", true);
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-	xhr.send(parameters);
+  var args = Array.prototype.slice.call(arguments, 2);
+  var xhr=createXHR();
+  var parameters="file=" + filename + "&name=" + user.trim()
+  for (var i = 0; i < args.length; i++)
+    parameters += "&a_" + i + "=" + args[i];
+  xhr.open("POST", "logger.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  xhr.send(parameters);
 }
 
 function postScore(gamename, user) {
@@ -46,15 +46,15 @@ function postScore(gamename, user) {
     parameters += "&column" + i + "=" + args[i];
   }
   xhr.open("POST", "scores", true);
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-	xhr.send(parameters);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  xhr.send(parameters);
 }
 
 function writecomment(filename, user, comment)
 {
-	var xhr = createXHR();
-	var parameters = "file=" + filename + "&name=" + user + "&comment=" + comment;
-	xhr.open("POST", "comment.php", true);
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-	xhr.send(parameters);
+  var xhr = createXHR();
+  var parameters = "file=" + filename + "&name=" + user + "&comment=" + comment;
+  xhr.open("POST", "comment.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  xhr.send(parameters);
 }
